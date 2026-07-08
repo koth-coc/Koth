11358acd96a5fca4687790a09f66ae9f19e50f05
koth-coc <rose.24.entp@gmail.com>
2026-07-08 17:49:43 +0530

    Update list.py

--- cogs/koth/commands/list.py
+++ cogs/koth/commands/list.py
@@ -12,7 +12,7 @@ def setup(group: app_commands.Group, bot):
             await interaction.response.send_message("No koths have been created yet.", ephemeral=True)
             return
 
-        embed = discord.Embed(title="KOTHs", color=discord.Color.white())
+        embed = discord.Embed(title="KOTHs", color=discord.Color.bruple())
         for k in koths:
             lines = [
                 f"Status: {k['status']}",
