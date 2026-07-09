f874fee5d31f977dc0dc7c8320455ce6a1839bec
koth-coc <rose.24.entp@gmail.com>
2026-07-09 17:44:55 +0530

    Update participants.py

--- cogs/koth/commands/participants.py
+++ cogs/koth/commands/participants.py
@@ -41,13 +41,13 @@ class ParticipantsView(discord.ui.View):
         embed.set_footer(text=f"Page {self.page + 1} of {self.max_page + 1}")
         return embed
 
-    @discord.ui.button(style=discord.ButtonStyle.secondary, emoji="<:Back:1524747897030180914>")
+    @discord.ui.button(style=discord.ButtonStyle.secondary, emoji="<:emoji_16:1524750436677189662>")
     async def previous_page(self, interaction: discord.Interaction, button: discord.ui.Button):
         self.page -= 1
         self.update_buttons()
         await interaction.response.edit_message(embed=self.build_embed(), view=self)
 
-    @discord.ui.button(style=discord.ButtonStyle.secondary, emoji="<:Next:1524747863521755326>")
+    @discord.ui.button(style=discord.ButtonStyle.secondary, emoji="<:emoji_15:1524750369908068423>")
     async def next_page(self, interaction: discord.Interaction, button: discord.ui.Button):
         self.page += 1
         self.update_buttons()
