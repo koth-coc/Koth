22a23f02327b530a609d9859aa6f1083d6e1b572
koth-coc <rose.24.entp@gmail.com>
2026-07-09 05:32:42 +0530

    Update embed_builder.py

--- cogs/koth/commands/embed_builder.py
+++ cogs/koth/commands/embed_builder.py
@@ -7,5 +7,11 @@ from ..utils import EmbedBuilderView
 def setup(group: app_commands.Group, bot):
     @group.command(name="builder", description="Build a custom embed with buttons")
     async def builder(interaction: discord.Interaction):
-        view = EmbedBuilderView(interaction.user.id)
-        await interaction.response.send_message(embed=view.embed, view=view)
+        guide_embed = discord.Embed(description="This will be the guide to how to make embed message")
+
+        view = EmbedBuilderView(interaction.user.id, default_channel=interaction.channel)
+        await interaction.response.send_message(embed=guide_embed, view=view, ephemeral=True)
+
+        preview_embed = discord.Embed(description="This message will be updated as we use the builder")
+        preview_msg = await interaction.followup.send(embed=preview_embed, ephemeral=True, wait=True)
+        view.preview_message = preview_msg
